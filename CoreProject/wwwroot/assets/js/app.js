ccadfdfe333301f58cf47d4c7ed28883d26aa390
Articole Technologies OPC Private Limited <admin@articoletech.com>
2019-04-13 23:59:59 +0530

    handeled null in app.js line 448

--- CoreProject/wwwroot/assets/js/app.js
+++ CoreProject/wwwroot/assets/js/app.js
@@ -445,13 +445,16 @@ function AjaxDisplayString() {
                 }
                 else {
                     var json = JSON.parse(data);
-                    $('.addressArea span').html(json[0].Address);//address
-                    $('.phoneNumber span').html(json[0].Landline1);//mobile
-                    $('.rLicNo').html(json[0].fssai);
-                    $('.email a').attr("href", "mailto: " + json[0].email);
-                    $('.email a').text(json[0].email);
-                    sessionStorage.setItem('OwnerEmail', json[0].email);
-                    sessionStorage.setItem('RestaurantDiscount', json[0].discount);
+                    if (json.length > 0) {
+                        $('.addressArea span').html(json[0].Address);//address
+                        $('.phoneNumber span').html(json[0].Landline1);//mobile
+                        $('.rLicNo').html(json[0].fssai);
+                        $('.email a').attr("href", "mailto: " + json[0].email);
+                        $('.email a').text(json[0].email);
+                        sessionStorage.setItem('OwnerEmail', json[0].email);
+                        sessionStorage.setItem('RestaurantDiscount', json[0].discount);
+                    }
+                    
                 }
             }
         });
